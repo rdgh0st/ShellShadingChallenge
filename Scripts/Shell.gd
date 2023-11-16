cf71a5ab960b415a1d5ee7959bb2e9e93774e9a6
rdgh0st <83750015+rdgh0st@users.noreply.github.com>
2023-11-16 14:54:28 -0700

    Clean Up

--- Scripts/Shell.gd
+++ Scripts/Shell.gd
@@ -24,19 +24,9 @@ var totalTime: float;
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	totalTime = -2;
-	# IDEA: add/subract from Texture itself before sending it to shader
 	
 	for i in ShellCount:
-		#set_instance_shader_parameter("ShellIndex", 100);
-		#print(get_instance_shader_parameter("ShellIndex"));
-		#self.get_active_material(0)
-		var normalMovement = 0;
-		var windMovement = 0;
-		for j in i:
-			var theta = (float(i) / float(ShellCount)) * (PI / 2) * WindIntensity;
-			normalMovement += ShellLength * cos(theta);
-			windMovement += ShellLength * sin(theta);
-		
+		# Create instance, attach copy of material, set initial parameters
 		shells.append(ShellChild.instantiate());
 		var shaderMat = shells[i].get_active_material(0).duplicate();
 		shaderMat.set_shader_parameter("ShellCount", float(ShellCount));
@@ -52,8 +42,6 @@ func _ready():
 		shaderMat.set_shader_parameter("ShellColor", ShellColor);
 		shaderMat.set_shader_parameter("Attenuation", Attenuation);
 		shaderMat.set_shader_parameter("OcclusionBias", OcclusionBias);
-		shaderMat.set_shader_parameter("NormalMovement", normalMovement);
-		shaderMat.set_shader_parameter("WindMovement", windMovement);
 		shells[i].set_surface_override_material(0, shaderMat);
 		add_child(shells[i]);
 		shells[i].set_owner(self);
@@ -65,47 +53,15 @@ func _process(delta):
 	if (totalTime >= 3):
 		totalTime = -2
 	
-	var velocity = 1.0;
-	var direction = Vector3(0, 0, 0);
-	var opposite = Vector3(0, 0, 0);
-	
-	direction.x = Input.get_axis("Left", "Right");
-	direction.y = Input.get_axis("Down", "Up");
-	direction.z = Input.get_axis("Backward", "Forward");
-	
-	direction = direction.normalized();
-	
-	self.position += direction * velocity * delta;
-	
-	displacementDirection -= direction * delta * 10;
-	if direction == Vector3.ZERO:
-		displacementDirection.y -= 10 * delta;
-	
-	if displacementDirection.length() > 1:
-		displacementDirection = displacementDirection.normalized();
-	
-	RenderingServer.global_shader_parameter_set("ShellDirection", displacementDirection);
 	RenderingServer.global_shader_parameter_set("WindDirection", WindDirection);
 	RenderingServer.global_shader_parameter_set("WindSource", self.position - WindDirection);
 	
-	#\sin^{2}\left(x\right)\ \frac{\sin\left(5x\right)}{2}+\ 0.5
-	#WindIntensity = (sin(totalTime) * sin(totalTime)) * (sin(5 * totalTime) / 2) + 0.5 + (sin(25 * totalTime) / 100);
-	
-	
-	WindIntensity = (2.0 / 1.67) * F((totalTime - 0.5) / 1.67) * P(10 * ((totalTime - 0.5) / 1.67)) + (sin(20 * totalTime) / 100.0);
+	# WindIntensity modeled after a function with sharp incline and gradual falloff, adding a sin wave to give it the appearance of some randomness
+	WindIntensity = (2.0 / 1.67) * F((totalTime - 0.5) / 1.67) * P(10 * ((totalTime - 0.5) / 1.67)) + (sin(10 * totalTime) / 50.0);
 	WindIntensity /= 1.5;
 	
-	
+	# Update shader parameters
 	for i in ShellCount:
-		#set_instance_shader_parameter("ShellIndex", 100);
-		#print(get_instance_shader_parameter("ShellIndex"));
-		#self.get_active_material(0)
-		#var normalMovement = 0;
-		#var windMovement = 0;
-		#for j in i:
-		#	var theta = (float(i) / float(ShellCount)) * (PI / 2) * WindIntensity;
-		#	normalMovement += ShellLength * cos(theta);
-		#	windMovement += ShellLength * sin(theta);
 		shells[i].get_active_material(0).set_shader_parameter("ShellCount", float(ShellCount));
 		shells[i].get_active_material(0).set_shader_parameter("ShellIndex", float(i));
 		shells[i].get_active_material(0).set_shader_parameter("ShellLength", ShellLength);
@@ -121,8 +77,6 @@ func _process(delta):
 		shells[i].get_active_material(0).set_shader_parameter("OcclusionBias", OcclusionBias);
 		shells[i].get_active_material(0).set_shader_parameter("WindIntensity", WindIntensity);
 		shells[i].get_active_material(0).set_shader_parameter("TotalTime", totalTime);
-		#shells[i].get_active_material(0).set_shader_parameter("NormalMovement", normalMovement);
-		#shells[i].get_active_material(0).set_shader_parameter("WindMovement", windMovement);
 
 func erf(x):
 	return 1.1 / (1 + 0.01 * exp(-3.2 * x));
